b9c90d63703b49699234a7bab74cb25042ccb067
Endy Muhardin <endy.muhardin@gmail.com>
2018-03-15 10:17:46 +0700

    fitur pencarian produk

--- belajar-springmvc/src/main/java/id/artivisi/belajar/springmvc/controller/ProdukController.java
+++ belajar-springmvc/src/main/java/id/artivisi/belajar/springmvc/controller/ProdukController.java
@@ -4,11 +4,14 @@ import id.artivisi.belajar.springmvc.dao.ProdukDao;
 import id.artivisi.belajar.springmvc.domain.Produk;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProdukController {
@@ -16,7 +19,8 @@ public class ProdukController {
     @Autowired ProdukDao produkDao;
     
     @GetMapping("/produk/list")
-    public void halo(HttpServletResponse response) throws IOException {
+    public void halo(@RequestParam(required = false)String cari,
+            HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         PrintWriter output = response.getWriter();
         output.println("<html><head><title>Data Produk</title></head>");
@@ -26,7 +30,13 @@ public class ProdukController {
         output.println("<table>");
         output.println("<tr><th>Id</th><th>Kode</th><th>Nama</th><th>Harga</th></tr>");
         
-        List<Produk> dataProduk = produkDao.semuaProduk();
+        List<Produk> dataProduk = new ArrayList<>();
+        if(StringUtils.hasText(cari)){
+            dataProduk.addAll(produkDao.cariProduk(cari));
+        } else {
+            dataProduk.addAll(produkDao.semuaProduk());
+        }
+        
         for(Produk p : dataProduk) {
             output.println("<tr>");
             output.println("<td>"+p.getId()+"</td>");
--- belajar-springmvc/src/main/java/id/artivisi/belajar/springmvc/dao/ProdukDao.java
+++ belajar-springmvc/src/main/java/id/artivisi/belajar/springmvc/dao/ProdukDao.java
@@ -2,6 +2,7 @@ package id.artivisi.belajar.springmvc.dao;
 
 import id.artivisi.belajar.springmvc.domain.Produk;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("produkDaoAsli")
 public class ProdukDao {
     
     @Autowired private DataSource dataSource;
@@ -32,6 +33,35 @@ public class ProdukDao {
                 hasil.add(p);
             }
             rs.close();
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        return hasil;
+    }
+    
+    public List<Produk> cariProduk(String cari){
+        List<Produk> hasil = new ArrayList<>();
+            
+        try {
+            String sql = "select * from produk where kode like ? or nama like ?";
+            Connection conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, "%"+cari+"%");
+            ps.setString(2, "%"+cari+"%");
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+                Produk p = new Produk();
+                p.setId(rs.getInt("id"));
+                p.setKode(rs.getString("kode"));
+                p.setNama(rs.getString("nama"));
+                p.setHarga(rs.getBigDecimal("harga"));
+                hasil.add(p);
+            }
+            rs.close();
+            ps.close();
             conn.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
