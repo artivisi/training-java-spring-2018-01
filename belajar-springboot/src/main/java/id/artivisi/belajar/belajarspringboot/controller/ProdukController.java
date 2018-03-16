d8d3e64ef79f53388f1dbb80c8c3107e5244d0c3
Endy Muhardin <endy.muhardin@gmail.com>
2018-03-16 14:42:03 +0700

    implement pagination

--- belajar-springboot/src/main/java/id/artivisi/belajar/belajarspringboot/controller/ProdukController.java
+++ belajar-springboot/src/main/java/id/artivisi/belajar/belajarspringboot/controller/ProdukController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -37,7 +38,7 @@ public class ProdukController {
     
     @GetMapping("/produk/list")
     public ModelMap halo(@RequestParam(required = false)String cari,
-            Pageable page) throws IOException {
+            @PageableDefault(sort = {"kode"}) Pageable page) throws IOException {
        
         ModelMap data = new ModelMap();
         
