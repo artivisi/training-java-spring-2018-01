82bffa4104912f930ecd59f4a7357a6840e96e7c
Endy Muhardin <endy.muhardin@gmail.com>
2018-03-16 14:17:16 +0700

    implement html tabel

--- belajar-springboot/src/main/java/id/artivisi/belajar/belajarspringboot/controller/ProdukController.java
+++ belajar-springboot/src/main/java/id/artivisi/belajar/belajarspringboot/controller/ProdukController.java
@@ -2,14 +2,20 @@ package id.artivisi.belajar.belajarspringboot.controller;
 
 import id.artivisi.belajar.belajarspringboot.dao.ProdukDao;
 import id.artivisi.belajar.belajarspringboot.domain.Produk;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -27,5 +33,21 @@ public class ProdukController {
     @ResponseStatus(HttpStatus.CREATED)
     public void simpan(@RequestBody Produk p){
         produkDao.save(p);
+    }
+    
+    @GetMapping("/produk/list")
+    public ModelMap halo(@RequestParam(required = false)String cari,
+            Pageable page) throws IOException {
+       
+        ModelMap data = new ModelMap();
+        
+        if(StringUtils.hasText(cari)){
+            data.addAttribute("dataProduk", 
+                    produkDao.findByKodeContainingIgnoreCaseOrNamaContainingIgnoreCase(cari, cari, page));
+        } else {
+            data.addAttribute("dataProduk", produkDao.findAll(page));
+        }
+        
+        return data;
     }
 }
--- belajar-springboot/src/main/java/id/artivisi/belajar/belajarspringboot/dao/ProdukDao.java
+++ belajar-springboot/src/main/java/id/artivisi/belajar/belajarspringboot/dao/ProdukDao.java
@@ -1,8 +1,10 @@
 package id.artivisi.belajar.belajarspringboot.dao;
 
 import id.artivisi.belajar.belajarspringboot.domain.Produk;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ProdukDao extends PagingAndSortingRepository<Produk, Integer> {
-    
+    Page<Produk> findByKodeContainingIgnoreCaseOrNamaContainingIgnoreCase(String kode, String nama, Pageable page);
 }
