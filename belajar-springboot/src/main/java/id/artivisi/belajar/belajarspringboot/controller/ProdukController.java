a770fb432fd3958f8cde3369f94ebcd6d73a0baf
Endy Muhardin <endy.muhardin@gmail.com>
2018-03-22 11:17:52 +0700

    fix import

--- belajar-springboot/src/main/java/id/artivisi/belajar/belajarspringboot/controller/ProdukController.java
+++ belajar-springboot/src/main/java/id/artivisi/belajar/belajarspringboot/controller/ProdukController.java
@@ -3,8 +3,6 @@ package id.artivisi.belajar.belajarspringboot.controller;
 import id.artivisi.belajar.belajarspringboot.dao.ProdukDao;
 import id.artivisi.belajar.belajarspringboot.domain.Produk;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
